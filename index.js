df05fcb8d2bde3e04a0a9ac262b64a497cf217d5
amc86 <amcuadrado1986@hotmail.com>
2022-11-26 12:23:57 +0100

    solucionado

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express'); 
 const dotenv = require('dotenv'); 
-const PORT = process.env.PORT || 9004;
+const PORT = process.env.PORT || 9007;
 const routerArtists = require('./src/api/routes/artists.routes');
 const routerConcerts = require('./src/api/routes/concerts.routes');
 const routerHalls = require('./src/api/routes/halls.routes');
 const routerGenres = require('./src/api/routes/genres.routes');
 const routerUsers = require('./src/api/routes/users.routes');
-const routerTickets = require('./src/api/routes/tickets.routes')
-
+const routerTickets = require('./src/api/routes/tickets.routes');
+const cors = require('cors');
 
 const {connect} = require('./utils/database'); //Importamos la conexión con la BBDD
 
@@ -15,9 +15,14 @@ connect(); //Conectamos después de iniciar el servidor con la BBDD
 dotenv.config();
 const app = express(); //Iniciamos el servidor con express
 
+app.use(cors({
+    origin: "*",
+    credentials: true
+})) // Use this after the variable declaration
 app.use(express.json()); //Necesario para poder usar json a la hora de enviar datos como puede ser con el método POST.
 app.use(express.urlencoded({ extended : false }));
 
+app.use('/', routerArtists, routerConcerts, routerHalls, routerGenres, routerUsers, routerTickets);
 app.use('/artists', routerArtists);
 app.use('/concerts', routerConcerts);
 app.use('/halls', routerHalls);
